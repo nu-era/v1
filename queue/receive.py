47550d9798a8f2b1d80770d26df1b466dbc525b1
kelley <kelley97@uw.edu>
2019-03-13 12:17:17 -0700

    Message sending to multiple consumers and json message

---
+++ queue/receive.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import pika,os
+
+url = os.environ.get('CLOUDAMQP_URL', 'http://ec2-54-68-59-121.us-west-2.compute.amazonaws.com')
+params = pika.URLParameters(url)
+connection = pika.BlockingConnection(params)
+channel = connection.channel()
+
+channel.exchange_declare(exchange='logs',
+                         exchange_type='fanout')
+
+result = channel.queue_declare(exclusive=True)
+queue_name = result.method.queue
+
+channel.queue_bind(exchange='logs',
+                   queue=queue_name)
+
+print(' [*] Waiting for logs. To exit press CTRL+C')
+
+def callback(ch, method, properties, body):
+    print(" [x] %r" % body)
+
+channel.basic_consume(callback,
+                      queue=queue_name,
+                      no_ack=True)
+
+channel.start_consuming()
