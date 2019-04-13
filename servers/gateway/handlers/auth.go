f42cbbea2e7b29a2bec498c87328c180edb29f95
Eric <ericjwei@gmail.com>
2019-04-13 00:14:21 -0700

    finished auth

--- servers/gateway/handlers/auth.go
+++ servers/gateway/handlers/auth.go
@@ -4,75 +4,184 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"strings"
+	"path"
+	"time"
 
 	"github.com/New-Era/servers/gateway/models/devices"
+	"github.com/New-Era/servers/gateway/sessions"
 )
 
-// Check request for 'Content-Type' header equal to the passed content type. If
-// not the correct content-type, returns error and writes 415 status response
-// to writer.
-func contentTypeCheck(w http.ResponseWriter, r *http.Request, contentT string) error {
-	contentType := r.Header.Get(headerContentType)
-	typeList := strings.Split(contentType, ",") // Get the first content type
-	if len(typeList) == 0 || typeList[0] != contentT {
-		http.Error(w, "Incorrect Content Type", http.statusUnsupportedMediaType)
+// DevicesHandler handles the creation (POST) of new devices. Primary key is stored using the name
+// of the device. A standard should be observed.
+func (ctx *HandlerContext) DevicesHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "method must be Post", http.StatusMethodNotAllowed)
 		return
 	}
-	return nil
+
+	if r.Header.Get(headerContentType) != contentTypeJSON {
+		http.Error(w, "content type must be application/json", http.StatusUnsupportedMediaType)
+		return
+	}
+
+	newDevice := &devices.NewDevice{}
+	if err := json.NewDecoder(r.Body).Decode(newDevice); err != nil {
+		http.Error(w, fmt.Sprintf("error decoding JSON: %v", err), http.StatusBadRequest)
+		return
+	}
+
+	device, err := newDevice.ToDevice()
+	if err != nil {
+		http.Error(w, fmt.Sprintf("error creating new device: %v", err), http.StatusBadRequest)
+		return
+	}
+
+	device, err = ctx.deviceStore.Insert(device)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("error adding device: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	newSession := &SessionState{StartTime: time.Now(), Device: device}
+	if _, err := sessions.BeginSession(ctx.signingKey, ctx.sessStore, newSession, w); err != nil {
+		http.Error(w, fmt.Sprintf("error creating new session: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	respond(w, device, http.StatusCreated)
 }
 
-// Post: registering a new device, create new session with device
-// Get: Create a new session for a registered device
+// SpecificUserHandler handles request for a specific device, requiring a prexisting sessions.
+// GET returns device info with given device name
+// PATCH updates the device name
+func (ctx *HandlerContext) SpecificDeviceHandler(w http.ResponseWriter, r *http.Request) {
+	sessionState := &SessionState{}
+	sessID, err := sessions.GetState(r, ctx.signingKey, ctx.sessStore, sessionState)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("problem with session %v", err), http.StatusUnauthorized)
+		return
+	}
+	deviceID := sessionState.Device.ID
+	switch r.Method {
+	case "GET":
+		device, err := ctx.deviceStore.GetByID(deviceID)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("device not found: %v", err), http.StatusNotFound)
+			return
+		}
+		respond(w, device, http.StatusOK)
+	case "PATCH":
+		segment := path.Base(r.URL.Path)
+		if segment != "me" && segment != deviceID.Hex() {
+			http.Error(w, "unathorized", http.StatusUnauthorized)
+		}
+		if r.Header.Get(headerContentType) != contentTypeJSON {
+			http.Error(w, "content type must be application/json", http.StatusUnsupportedMediaType)
+			return
+		}
 
-func (ctx *HandlerContext) DevicesHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" && r.Method != "GET" {
-		http.Error(w, "method must be Post or Get", http.statusMethodNotAllowed)
+		device, err := ctx.deviceStore.GetByID(deviceID)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("error getting device: %v", err), http.StatusNotFound)
+		}
+
+		deviceUpdates := &devices.Updates{}
+		if err := json.NewDecoder(r.Body).Decode(deviceUpdates); err != nil {
+			http.Error(w, fmt.Sprintf("error decoding JSON: %v", err), http.StatusBadRequest)
+			return
+		}
+
+		if err = device.ApplyUpdates(deviceUpdates); err != nil {
+			http.Error(w, fmt.Sprintf("error updating device: %v", err), http.StatusBadRequest)
+			return
+		}
+
+		if err = ctx.deviceStore.Update(deviceID, deviceUpdates); err != nil {
+			http.Error(w, fmt.Sprintf("error updating device: %v", err), http.StatusBadRequest)
+			return
+		}
+
+		sessionState.Device = device
+		if err := ctx.sessStore.Save(sessID, sessionState); err != nil {
+			http.Error(w, fmt.Sprintf("error updating session state: %v", err), http.StatusInternalServerError)
+			return
+		}
+		respond(w, device, http.StatusOK)
+	default:
+		http.Error(w, "method must be GET or PATCH", http.StatusMethodNotAllowed)
+		return
+	}
+}
+
+//SessionsHandler handels requests for the sessions resource. POST allows for logging in with creditials.
+func (ctx *HandlerContext) SessionsHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "method must be POST", http.StatusMethodNotAllowed)
+		return
+	}
+	if r.Header.Get(headerContentType) != contentTypeJSON {
+		http.Error(w, "content type must be application/json", http.StatusUnsupportedMediaType)
 		return
 	}
 
-	if r.Method == http.MethodPost {
-		// Check to make sure content-type is application/json
-		err := contentTypeCheck(w, r, contentTypeJSON)
-		if err != nil {
-			return
-		}
+	deviceCredentials := &devices.Credentials{}
+	if err := json.NewDecoder(r.Body).Decode(deviceCredentials); err != nil {
+		http.Error(w, fmt.Sprintf("error decoding JSON: %v", err), http.StatusBadRequest)
+		return
+	}
+	device, err := ctx.deviceStore.GetByName(deviceCredentials.Name)
+	if device == nil { //Set dummy device
+		device = &devices.Device{}
+	}
+	err = device.Authenticate(deviceCredentials.Password)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("invalid credentials"), http.StatusUnauthorized)
+		return
+	}
+	newSession := &SessionState{StartTime: time.Now(), Device: device}
+	if _, err := sessions.BeginSession(ctx.signingKey, ctx.sessStore, newSession, w); err != nil {
+		http.Error(w, fmt.Sprintf("error creating new session: %v", err), http.StatusInternalServerError)
+		return
+	}
 
-		newDevice := devices.NewDevice{} // Create an empty NewDevice struct to be filled by request body
-		decoder := json.NewDecoder(r.Body)
-		if err := decoder.Decode(&newDevice); err != nil {
-			http.Error(w, "Request body unable to be decoded to new device", 400)
-			return
-		}
+	// ip := r.RemoteAddr
+	// ips := strings.Split(r.Header.Get(headerXForwarded), ", ")
+	// if len(ips) > 0 {
+	// 	ip = ips[0]
+	// }
+	// if err := ctx.usersStore.Log(user.ID, ip); err != nil {
+	// 	http.Error(w, fmt.Sprintf("error recording ip of user: %v", err), http.StatusBadRequest)
+	// 	return
+	// }
+	respond(w, device, http.StatusCreated)
+}
 
-		validDevice, err := newDevice.ToDevice() // Turn new device to a Device type
-		if err != nil {
-			http.Error(w, err.Error(), 400)
-			return
-		}
+//SpecificSessionHandler handles requests related to a specific authenticated session
+func (ctx *HandlerContext) SpecificSessionHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "DELETE" {
+		http.Error(w, "method must be DELETE", http.StatusMethodNotAllowed)
+		return
+	}
+	if segment := path.Base(r.URL.Path); segment != "mine" {
+		http.Error(w, "unknown path segment", http.StatusForbidden)
+		return
+	}
+	if _, err := sessions.EndSession(r, ctx.signingKey, ctx.sessStore); err != nil {
+		http.Error(w, fmt.Sprintf("session not found: %v", err), http.StatusBadRequest)
+		return
+	}
+	fmt.Println("signed out")
+	respond(w, nil, http.StatusOK)
+	return
+}
 
-		// Insert the new device to db, and get the newly inserted device
-		// with the new database-assigned primary key value
-		validDevice, err = ctx.DeviceStore.Insert(validDevice)
-		if err != nil {
-			http.Error(w, err.Error(), 400)
-			return
-		}
-
-		err = ctx.WebSocketConnectionHandler(w, r, validDevice.ID)
-		if err != nil {
-			return
-		}
-		conn := ctx.WsConnections.Conns[validDevice.ID]
-		go heartbeat(conn)
-
-		// Respond to the client with an http.StatusCreated code, and
-		// the json encoded new user profile
-		w.Header().Add("Content-Type", "application/json")
-		w.WriteHeader(201)
-		err = json.NewEncoder(w).Encode(validDevice)
-		if err != nil {
-			http.Error(w, err.Error(), 400)
+//respond responds with the status, content type of JSON and encoded value
+func respond(w http.ResponseWriter, value interface{}, status int) {
+	w.WriteHeader(status)
+	if value != nil {
+		w.Header().Add(headerContentType, contentTypeJSON)
+		if err := json.NewEncoder(w).Encode(value); err != nil {
+			http.Error(w, fmt.Sprintf("error encoding response value to JSON: %v", err), http.StatusInternalServerError)
 			return
 		}
 	}
--- servers/gateway/handlers/context.go
+++ servers/gateway/handlers/context.go
@@ -2,20 +2,22 @@ package handlers
 
 import (
 	"github.com/New-Era/servers/gateway/models/devices"
+	"github.com/New-Era/servers/gateway/sessions"
 )
 
 // HandlerContext tracks the key that is used to sign and
 // validate SessionIDs, the sessions.Store, and the users.Store
 //Context holds contex values for multiple handler functions
 type HandlerContext struct {
-	signingKey  string
-	sessStore   sessions.Store
+	signingKey    string
+	sessStore     sessions.RedisStore
+	deviceStore   devices.MongoStore
 	WsConnections *Connections
 }
 
 //NewHandlerContext constructs a new HandlerContext,
 //ensuring that the dependencies are valid values
-func NewHandlerContext(signingKey string, sessStore sessions.Store, deviceStore device.Store, connections *Connections) *Context {
+func NewHandlerContext(signingKey string, sessStore *sessions.RedisStore, deviceStore *devices.MongoStore, connections *Connections) *HandlerContext {
 	if signingKey == "" {
 		panic("empty signing key")
 	}
@@ -26,9 +28,9 @@ func NewHandlerContext(signingKey string, sessStore sessions.Store, deviceStore 
 		panic("nil device store")
 	}
 	return &HandlerContext{
-		signingKey:  signingKey,
-		sessStore:   sessStore,
-		deviceStore:  deviceStore,
-		WsConnections *Connections,
+		signingKey:    signingKey,
+		sessStore:     *sessStore,
+		deviceStore:   *deviceStore,
+		WsConnections: connections,
 	}
 }
