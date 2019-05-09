8487f15e49bb426f559cc90f1cacda77c4bf9329
kelley <kelley97@uw.edu>
2019-05-09 16:01:56 -0700

    added disconnect time

--- tests/stressTests.js
+++ tests/stressTests.js
@@ -4,16 +4,45 @@
   // let time1;
   // let time2;
   function init() {
-    // createDevice(null);
+    // createDevice(null, null);
     // deviceLogin();
-    deviceInfo();
-
+    // deviceInfo();
+    deviceDisconnect();
   }
 
+  function deviceDisconnect() {
+    createDevice(connectDevice, disconnect);
+  }
   function deviceInfo() {
-    createDevice(getInfo);
+    createDevice(getInfo, null);
   }
 
+  function disconnect(data, bearer) {
+    let time1 = performance.now()
+    let settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://api.bfranzen.me/disconnect",
+      "method": "DELETE",
+      "headers": {
+        "Content-Type": "application/json",
+        "Authorization": bearer
+        // "cache-control": "no-cache",
+        // "Postman-Token": "be187d81-5a0f-4958-b507-de958572128d"
+      },
+      "processData": false,
+      "data": data
+    }
+    $.ajax(settings).done(function (response) {
+      let time2 = performance.now();
+      let bd = document.querySelector("body");
+      let p = document.createElement("p");
+      p.setAttribute("id", "disconnect-time");
+      p.innerText = "Time to disconnect is " + (time2 - time1) / 1000 + " seconds";
+      bd.appendChild(p);
+      console.log(response);
+    });
+  }
   function getInfo(bearer, deviceName) {
     let time1 = performance.now();
     var settings = {
@@ -32,8 +61,11 @@
     $.ajax(settings).done(function (response) {
       let time2 = performance.now();
       console.log(response);
-      let p = document.getElementById("time-info");
-      p.innerHTML = "Getting device info took " + ((time2 - time1)/1000) + " seconds.";
+      let bd = document.querySelector("body");
+      let p = document.createElement("p");
+      p.setAttribute("id", "getinfo-time");
+      p.innerText = "Getting device info took " + ((time2 - time1)/1000) + " seconds.";
+      bd.appendChild(p);
     });
   }
   // Creates a device then logs in
@@ -42,8 +74,9 @@
   }
 
   // Makes ajax call to the connect endpoint to connect/login to a device
-  function connectDevice(bearer, deviceName) {
+  function connectDevice(bearer, deviceName, nextCall) {
     let time1 = performance.now();
+    let data = "{\n\t\"name\": \""+ deviceName + "\",\n\t\"latitude\": 123,\n\t\"longitude\": 234,\n\t\"email\": \"tesst@test\",\n\t\"phone\": \"12345\",\n\t\"password\": \"1234567\",\n\t\"passwordConf\": \"1234567\"\n}";
     let settings = {
       "async": true,
       "crossDomain": true,
@@ -56,25 +89,34 @@
         // "Postman-Token": "a666feca-010b-4179-8bf4-a23c5b4846bd"
       },
       "processData": false,
-      "data": "{\n\t\"name\": \""+ deviceName + "\",\n\t\"latitude\": 123,\n\t\"longitude\": 234,\n\t\"email\": \"tesst@test\",\n\t\"phone\": \"12345\",\n\t\"password\": \"1234567\",\n\t\"passwordConf\": \"1234567\"\n}"    }
+      "data": data
+    }
 
     $.ajax(settings).done(function (response) {
       let time2 = performance.now();
       console.log(response);
-      let p = document.getElementById("time-info");
-      p.innerHTML = "Logging into a device took " + ((time2 - time1)/1000) + " seconds.";
+      if (nextCall === null) {
+        let bd = document.querySelector("body");
+        let p = document.createElement("p");
+        p.setAttribute("id", "connect-time");
+        p.innerText = "Logging into a device took " + ((time2 - time1)/1000) + " seconds.";
+        bd.appendChild(p);
+
+      } else {
+        nextCall(data, bearer);
+      }
     });
   }
 
   // Sends ajax request to create a device, if login is true
   // it will use credentials to login
-  function createDevice(func) {
-    let baseName = "loginTest";
+  function createDevice(func, nextCall) {
+    let baseName = "timeDisconnect1";
     let time1 = performance.now();
     // let time2 = performance.now();
     let numDevices = 1;
     for (let i = 0; i < numDevices; i++) {
-      let deviceName = "testDeviceInfo05";
+      let deviceName = "timeDisconnect5";
       let settings = {
         "async": true,
         "crossDomain": true,
@@ -84,7 +126,7 @@
           "Content-Type": "application/json",
         },
         "processData": false,
-        "data": "{\n\t\"name\": \""+deviceName+"\",\n\t\"latitude\": 123,\n\t\"longitude\": 234,\n\t\"email\": \"tesst@test\",\n\t\"phone\": \"12345\",\n\t\"password\": \"1234567\",\n\t\"passwordConf\": \"1234567\"\n}"
+        "data": "{\n\t\"name\": \""+ deviceName +"\",\n\t\"latitude\": 123,\n\t\"longitude\": 234,\n\t\"email\": \"tesst@test\",\n\t\"phone\": \"12345\",\n\t\"password\": \"1234567\",\n\t\"passwordConf\": \"1234567\"\n}"
       }
       let jqxhr = $.ajax(settings)
       jqxhr.done(function (response) {
@@ -92,10 +134,13 @@
         console.log(response);
         let bearer = jqxhr.getResponseHeader("Authorization");
         if (func !== null) {
-          func(bearer, deviceName);
+          func(bearer, deviceName, nextCall);
         } else {
-          let p = document.getElementById("time-info");
-          p.innerHTML = "Creating "+ numDevices + " devices took " + ((time2 - time1)/1000) + " seconds.";
+          let bd = document.querySelector("body");
+          let p = document.createElement("p");
+          p.setAttribute("id", "createdevice-time");
+          p.innerText = "Creating a device took " + ((time2 - time1)/1000) + " seconds.";
+          bd.appendChild(p);
         }
       });
     }
