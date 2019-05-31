9ac16397fabd2616d9ac237d90fdecb99dff49d8
bfranzen1 <bef6823@gmail.com>
2019-05-31 13:47:22 -0700

    remove checkserver

--- servers/gateway/main.go
+++ servers/gateway/main.go
@@ -179,29 +179,3 @@ func failOnError(err error, msg string) {
 		log.Fatalf("%s: %s", msg, err)
 	}
 }
-
-func checkServer() {
-	conn, err := net.Dial("tcp", "alert1.eew.shakealert.org:61617")
-	if err != nil {
-		fmt.Println("dial error:", err)
-		return
-	}
-	defer conn.Close()
-	fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
-
-	buf := make([]byte, 0, 4096) // big buffer
-	tmp := make([]byte, 256)     // using small tmp buffer for demonstrating
-	for {
-		n, err := conn.Read(tmp)
-		if err != nil {
-			if err != io.EOF {
-				fmt.Println("read error:", err)
-			}
-			break
-		}
-		fmt.Println("got", n, "bytes.")
-		buf = append(buf, tmp[:n]...)
-		fmt.Println("total size:", len(buf))
-		fmt.Println(string(buf))
-	}
-}
